6ce0746e5c5c25cf86f2790fc063082e9337a389
서보성 <sbosung0610@gmail.com>
2026-03-09 20:37:18 +0900

    테스트 코드 수정

--- groundingLMM/test_glamm.py
+++ groundingLMM/test_glamm.py
@@ -244,8 +244,7 @@ def main():
                 use_cache=True,
                 bad_words_ids=[[tokenizer.unk_token_id]],
                 do_sample=False,
-                num_beams=1,
-                repetition_penalty=1.15
+                num_beams=1
                 )
             raw_text = tokenizer.decode(output_ids[0, input_ids.shape[1]:], skip_special_tokens=False)
 
