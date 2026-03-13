2c09432e5ed7831cae30f352994914b6de83fde5
서보성 <sbosung0610@gmail.com>
2026-03-13 12:31:19 +0900

    테스트 설정 수정

--- groundingLMM/test_glamm.py
+++ groundingLMM/test_glamm.py
@@ -80,7 +80,7 @@ class ForestEvalDataset(Dataset):
 
         conv = conversation_lib.conv_templates["llava_v1"].copy()
         conv.append_message(conv.roles[0], DEFAULT_IMAGE_TOKEN + "\n" + clean_q)
-        conv.append_message(conv.roles[1], "Based on the analysis, the results are as follows: In this area, there is") 
+        conv.append_message(conv.roles[1], "Based on the analysis, the results are as follows: In this area, there is a cluster of ") 
         
         # 프롬프트 문자열을 뽑아낸 뒤, 끝에 몰래 붙은 종료 토큰을 완전히 뜯어냄
         prompt_str = conv.get_prompt()
@@ -243,8 +243,9 @@ def main():
                 max_new_tokens=256, 
                 use_cache=True,
                 bad_words_ids=[[tokenizer.unk_token_id]],
-                do_sample=False,
-                num_beams=1
+                do_sample=True,
+                temperature=0.2,
+                top_p=0.9
                 )
             raw_text = tokenizer.decode(output_ids[0, input_ids.shape[1]:], skip_special_tokens=False)
 
