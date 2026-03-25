d3b0e53255e359d97b03ffd2a4503c98f22d28ab
서보성 <sbosung0610@gmail.com>
2026-03-25 10:47:15 +0900

    CarbonLoss 클래스 테스트에 맞게 수정

--- u_net/model/metrics.py
+++ u_net/model/metrics.py
@@ -6,7 +6,6 @@ import numpy as np
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from evaluate import corr, r_square, corr_wZero, r_square_wZero
 
-
 class CarbonLoss(nn.Module):
     def __init__(self, weight=None, size_average=True, cls_lambda=50, reg_lambda=0.005, ignore_label=255):
         super().__init__()
@@ -15,34 +14,43 @@ class CarbonLoss(nn.Module):
         self.cls_lambda = cls_lambda
         self.reg_lambda = reg_lambda
 
-    def forward(self, input_cls,input_reg, target_cls, target_reg):
-                
-        cls_loss = self.ce(input_cls, target_cls.type(torch.long).squeeze())
+    def forward(self, input_cls, input_reg, target_cls, target_reg):
         
+        cls_loss = self.ce(input_cls, target_cls.type(torch.long).view(input_cls.size(0), input_cls.size(2), input_cls.size(3)))
+
         reg_loss = self.mse(
             torch.flatten(input_reg, end_dim=-2),
             torch.flatten(target_reg, end_dim=-2)
         )
-        input_reg = input_reg.squeeze()
-        target_reg = target_reg.squeeze()
+        
+        input_reg = input_reg.squeeze(1)
+        target_reg = target_reg.squeeze(1)
 
-        corr_sum=0
-        r_sum =0
+        corr_sum = 0
+        r_sum = 0
         idx = 0
+        
         for i in range(len(input_reg)):
             ir = input_reg[i,...].cpu().detach().numpy()
             tr = target_reg[i,...].cpu().detach().numpy()
+            
+            # 정답 텐서에 유효한 값이 없으면 스킵
             if np.count_nonzero(tr) == 0:
                 continue          
+            
             corr_res = corr_wZero(ir,tr)
             r_res = r_square_wZero(ir,tr)
+            
             if np.isnan(corr_res) or np.isnan(r_res):           
                 continue
+                
             corr_sum = corr_sum + corr_res
             r_sum = r_sum + r_res
-            idx = idx +1
-        acc_c = corr_sum/idx
-        acc_r = r_sum/idx
+            idx = idx + 1
+            
+  
+        acc_c = corr_sum / idx if idx > 0 else 0.0
+        acc_r = r_sum / idx if idx > 0 else 0.0
 
         total_loss = self.cls_lambda * cls_loss + self.reg_lambda * reg_loss
 
